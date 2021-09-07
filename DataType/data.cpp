a7a8ffef02ea3aec430f275868d5bedd58401ce2
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-09-07 16:27:22 +0545

    Simple Datatype Declare

---
+++ DataType/data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string name = "Siddhartha";
+    int age = 20; 
+    double gpa = 4.5;
+    bool isHot = true;
+    cout << "Hello" << endl;
+}
