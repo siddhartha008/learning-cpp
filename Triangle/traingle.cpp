ad2a46d1d2b6e2c24ecbaf76564735f360f619da
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-09-07 16:40:36 +0545

    Hardcoded triangle

---
+++ Triangle/traingle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+//this is a program that prints a hardcoded right angled triangle
+int main() {
+    cout << "    /|" << endl;
+    cout << "   / |" << endl;
+    cout << "  /  |" << endl;
+    cout << " /   |" << endl;
+    cout << "/____|" << endl;
+    return 0;
+}
+
+
