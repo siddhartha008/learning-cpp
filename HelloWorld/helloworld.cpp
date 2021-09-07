0f00e08f6c1678079f8a7ab451eb163a87e7a09d
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-09-07 16:22:15 +0545

    First program: Hello World

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    //a program to print hello world in CPP
+    cout << "Hello, world!" << endl;
+    
+    //we use 'cout' to output in CPP
+    return 0;
+}
