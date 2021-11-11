c3b5f63f0b27a197960975d5ef2599c109e276dd
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-11-11 12:12:47 +0545

    even_odd

---
+++ even_odd/even_odd.cpp
@@ -0,0 +1,18 @@
+//User enters an integer number
+//Our program will tell if the number is even or odd. 
+
+#include <iostream> 
+
+using namespace std;
+
+int main() { 
+    int number; 
+    cout << "Enter a number: "; 
+    cin >> number; 
+    if (number % 2 == 0) { 
+        cout << "The number " << number << " is even." << endl; 
+    } else {
+        cout << "The number " << number << " is odd." << endl; 
+    }
+    return 0;
+}
