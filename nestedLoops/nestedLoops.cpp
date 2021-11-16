92cdbbe0d2883563f1b2f2f594e666e481ed3fbe
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-11-16 13:30:06 +0545

    nested loop

---
+++ nestedLoops/nestedLoops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int grade, length, sum=0;
+
+    cout << "How many grades do you want to enter? ";
+    cin >> length; 
+
+    for (int i=0; i<length; i++) { 
+        do {
+            cout << "Enter grade(" << (i+1) << "): ";
+            cin >> grade;
+        }while(grade < 1 || grade > 5);
+        sum += grade;
+    }
+
+    cout << "Sum of the accepted grades: " << sum << endl;
+
+    return 0;
+}
