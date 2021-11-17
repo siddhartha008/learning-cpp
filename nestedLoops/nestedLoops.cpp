89992376eb3335953d0490bca5a9b808f9641a9f
Sid <83157044+siddhartha008@users.noreply.github.com>
2021-11-17 07:21:43 +0545

    multiplication table

--- nestedLoops/nestedLoops.cpp
+++ nestedLoops/nestedLoops.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
-    int grade, length, sum=0;
+int main()
+{
+    int grade, length, sum = 0;
 
     cout << "How many grades do you want to enter? ";
-    cin >> length; 
+    cin >> length;
 
-    for (int i=0; i<length; i++) { 
-        do {
-            cout << "Enter grade(" << (i+1) << "): ";
+    for (int i = 0; i < length; i++)
+    {
+        do
+        {
+            cout << "Enter grade(" << (i + 1) << "): ";
             cin >> grade;
-        }while(grade < 1 || grade > 5);
+        } while (grade < 1 || grade > 5);
         sum += grade;
     }
 
